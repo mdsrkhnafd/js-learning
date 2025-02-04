2c00cdad7fd66c963315acee0d4e2011e005f9c2
Mudasir Khan <72373676+mdsrkhnafd@users.noreply.github.com>
2025-02-04 10:39:10 +0000

    datatypes summery

---
+++ 01_basics/datatypes-summery.js
@@ -0,0 +1,17 @@
+// Primative
+
+// 7 Types: String, Number , Boolean , null , undefined , Symbol , BigInt
+
+
+// Reference (Non Primative)
+
+// Array Objects Functions
+
+let myArray = ["Mudasir" , "Waqas" , "Waqar" , "Osama"];
+
+let myObject = {
+    name: "Mudasir",
+    age: 27
+};
+
+let myFunction = function () {}
