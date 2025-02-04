f234b7a5a134f402d7f6907afb3ecd3b90152642
Mudasir Khan <72373676+mdsrkhnafd@users.noreply.github.com>
2025-02-04 06:25:01 +0000

    learn variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 234353;
+let acountEmail = "mudasir@gmail.com";
+var accountPassword = "432545657";
+acountCity = "Islamabad";
+
+// accountId = 34523;
+
+console.log(accountId);
+
+acountEmail = "mk@gmail.com";
+accountPassword = "23445";
+acountCity = 'Kohat'
+
+console.table([accountId , acountEmail , accountPassword , acountCity]);
