97664e9df76d82d89228be16a805f4383b464b7a
Mudasir Khan <72373676+mdsrkhnafd@users.noreply.github.com>
2025-02-04 08:00:08 +0000

    Conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,37 @@
+let score = "33abc";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 , false => 0
+// null => 0
+// undefined => NaN
+
+
+let isLoggedIn = "mudasir";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true , 0 => false
+// "" => false
+// "mudasir" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+// 33 => 33
+
+
+
+
